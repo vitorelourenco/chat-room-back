4fa101678215969f6e986e6570122edda39210ef
Vitor Lourenco <vitor.registros@lourencos.net>
2021-06-10 18:23:48 -0300

    Add post message functionality

--- src/app.js
+++ src/app.js
@@ -36,6 +36,52 @@ app.get("/messages",(req,res)=>{
   const messages = getMessages();
 });
 
+app.post("/messages", (req,res)=>{
+  const body = req.body;
+  const headers = req.headers;
+
+  if (typeof body.to !== "string"){
+    res.status(400).send("Invalid input type <to>");
+    return;
+  }
+  if (typeof body.text !== "string"){
+    res.status(400).send("Invalid input type <text>");
+    return;
+  }
+  if (typeof body.type !== "string"){
+    res.status(400).send("Invalid input type <type>");
+    return;
+  }
+  if (typeof headers.user !== "string"){
+    res.status(400).send("Invalid input type <user>");
+    return;
+  }
+
+  const to = stripHtml(body.to).result.trim();
+  const text = stripHtml(body.text).result.trim();
+  const type = stripHtml(body.type).result.trim();
+  const user = stripHtml(headers.user).result.trim();
+
+  if (!(type==="message"||type==="private_message")){
+    res.status(400).send("Invalid input message <type>");
+    return;   
+  }
+
+  const participants = getParticipants();
+  if (participants.indexOf(user) === -1){
+    res.status(400).send("Sender is not on the list");
+    return;   
+  }
+
+  const messages = getMessages();
+  const time = dayjs(Date.now()).format("HH-mm-ss");
+  const message = {from: user, to, text, type, time}
+  messages.push(message);
+  setMessages(messages);
+
+  res.status(200).send("OK");
+})
+
 app.get("/participants",(req,res)=>{
   const participants = getParticipants();
   res.status(200).send(participants);
@@ -48,7 +94,7 @@ app.post("/participants",(req,res)=>{
     return;
   }
 
-  const name = stripHtml(body.name).result;
+  const name = stripHtml(body.name).result.trim();
   if (name === "") {
     res.status(400).send("Name is empty");
     return;
@@ -66,8 +112,7 @@ app.post("/participants",(req,res)=>{
 
   const messages = getMessages();
   const time = dayjs(lastStatus).format("HH-mm-ss");
-  console.log(time);
-  const message = {name, to:"Todos", text:"entra na sala...", type:"status", time}
+  const message = {from: name, to:"Todos", text:"entra na sala...", type:"status", time}
   messages.push(message);
   setMessages(messages);
 
